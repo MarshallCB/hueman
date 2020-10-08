dfdb8f1da4e09676a631586a52c8fe3451c23cab
Marshall Brandt <MarshallCB@users.noreply.github.com>
2020-10-08 10:40:48 -0500

    added alpha, edited formula

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
 let fix = (n) => +n.toFixed(2)
+let rawmix = (x, y, ratio) => x * (1-ratio) + y * ratio
+let fence = (min,x,max) => Math.max(Math.min(max,x),min)
 
-export function mix(zero, one, ratio = 0.5){
-  zero = zero % 360
-  one = one % 360
-  if(Math.abs(zero - one) >= 180){
+export function mix(x, y, ratio = 0.5){
+  x = x % 360
+  y = y % 360
+  if(Math.abs(x - y) >= 180){
     // if they're closer wrapped, wrap the lower across 360
-    if(zero < one){
-      zero = zero + 360
+    if(x < y){
+      x = x + 360
     } else {
-      one = one + 360
+      y = y + 360
     }
   }
   // Perform ratio, and mod by 360 in case of wrap
-  return Math.round((zero * (1-ratio) + one * ratio)) % 360
+  return Math.round(rawmix(x,y,ratio)) % 360
 }
+
 
 let scores = [
   {
@@ -82,19 +85,35 @@ let scores = [
 // multiply hsl values by inverse ratio compared to 50/50
 
 export function correction(hue){
-  let p = (hue % 360) / 30 - 0.5 // hue --> [0.5, 12.5] (because we have "middle" values (starts at 15, 30 increments))
+  hue = hue % 360
+  let p = hue / 30 - 0.5 // hue --> [0.5, 12.5] (because we have "middle" values (starts at 15, 30 increments))
   let l = p - Math.floor(p); // decimal distance from Math.floor
   let r = 1 - l; // decimal distance from Math.ceil
   let left = scores[(Math.floor(p) + 12) % 12]
   let right = scores[(Math.ceil(p)) % 12]
-  
+
+  let position = {
+    chroma: (left.chroma * l + right.chroma * r),
+    lightness: (left.lightness * l + right.lightness * r),
+    hue: (left.hue * l + right.hue * r)
+  }
   // using 210 hue as base (where 49 and 56 come from)
+  let chromaCoef = 49 / position.chroma
+  let lightnessCoef = 56 / position.lightness
   return { 
-    chroma: fix(49 / (left.chroma * l + right.chroma * r)),
-    lightness: fix(56 / (left.lightness * l + right.lightness * r))
+    chroma: fix(chromaCoef),
+    lightness: fix(lightnessCoef)
   }
 }
-export function man(h,s,l){
-  let { chroma, lightness } = correction(h)
-  return `hsl(${h}, ${fix(Math.min(100,s * chroma))}%, ${fix(Math.min(l * lightness))}%)`
+export function man(h,s=0.5,l=0.5, a=1.0){
+  h = h % 360
+  s = fence(0, s, 1)
+  l = fence(0, l, 1)
+  let { chroma, lightness } = correction(h,s,l)
+  let weight = 1 - (Math.abs(1.0 - s) * (1 - Math.abs(0.5 - l) * 2))
+  let hue = h
+  let sat = fence(0,rawmix(s* 100, s * 100 * chroma , weight),100)
+  let lit = fence(0,rawmix(l* 100, l * 100 * lightness, weight ),100)
+  let alpha = a
+  return `hsla(${hue}, ${fix(sat)}%, ${fix(lit)}%, ${fix(alpha)})`
 }
